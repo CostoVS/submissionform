f7db978c91b41f5881adbbc43afcf5eb8b99285f
Nicholaus C Chetty <nicholauscostochetty@gmail.com>
2023-08-22 22:46:21 +0200

    submission form
    sub form

---
+++ submit.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
+    
+    $to = "mailtestingsubmission@gmail.com"; // Replace with your email address
+    $subject = "New Submission from your website";
+    $headers = "From: $email";
+    
+    $mailBody = "Name: $name\n";
+    $mailBody .= "Email: $email\n";
+    $mailBody .= "Phone: $phone\n\n";
+    $mailBody .= "Message:\n$message";
+    
+    if (mail($to, $subject, $mailBody, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error sending the message.";
+    }
+}
+?>
